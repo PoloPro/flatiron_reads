07fe13f31893a09d6cbb58d7c41cd6aa02b2e2d1
PoloPro <wesleybdavis@gmail.com>
2016-05-05 17:33:31 -0400

    added book protection

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   # t.string :name
   has_many :books
-  accepts_nested_attributes_for :books
+  accepts_nested_attributes_for :books, reject_if: :all_blank
 
   validates :name, presence: true, uniqueness: true
 end
